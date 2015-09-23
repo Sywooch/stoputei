ca92cf8184cfd35fcfdeef0a242f643318b1fd2e
alex_azbukin <azbukin777@gmail.com>
2015-09-23 12:25:56 +0300

    change db

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 // password on real for this environment
     return [
         'class' => 'yii\db\Connection',
-        'dsn' => 'mysql:host=localhost;dbname=stoputei',
-        'username' => 'us_stoputei',
+        'dsn' => 'mysql:host=localhost;dbname=stoputei_prod',
+        'username' => 'us_stoputei_prod',
         'password' => 'QazWsxEdc1234',
         'charset' => 'utf8',
     ];
