8f6df4b7334bacbca307b8f237d92ef23b96eaa5
alex_azbukin <azbukin777@gmail.com>
2015-10-14 16:56:24 +0300

    add cron task tour

--- commands/TourController.php
+++ commands/TourController.php
@@ -8,6 +8,7 @@
 namespace app\commands;
 
 use yii\console\Controller;
+use app\modules\admin\models\TimeCycles;
 
 /**
  * This command echoes the first argument that you have entered.
@@ -28,7 +29,12 @@ class TourController extends Controller
         $file = 'web/list.txt';
         $fh = fopen($file, 'a') or die("can't open file");
         $longDate = \Yii::$app->formatter->format('now', 'datetime');
-        fwrite($fh, 'Write text at '.$longDate. "\n");
+        $timeCycle = TimeCycles::find()->where(['is not', 'id', null])->one();
+        $tour_request_life = $timeCycle->tour_request_life;
+        $tour_response_life = $timeCycle->tour_response_life;
+        $flight_request_life = $timeCycle->flight_request_life;
+        $flight_response_life = $timeCycle->flight_response_life;
+        fwrite($fh, "Tour request life: $timeCycle->tour_request_life , Tour response life: $timeCycle->tour_response_life , Flight request life: $timeCycle->flight_request_life , Flight response life: $timeCycle->flight_response_life .... Write text at $longDate \n");
         fclose($fh);
         echo $message . "\n";
         echo 'TEST console controller' . "\n";
